d9c019d62bed1ed71f9c4a441c0fb814ffa3a730
AntonisEkatommatis <antonismatis@gmail.com>
2019-02-12 21:32:55 +0200

    Άσκηση 3 | Εργασίες Εξαμήνου
    Αντώνης Εκατομμάτης
    ΕΙΣΑΓΩΓΗ ΣΤΗΝ ΕΠΙΣΤΗΜΗ ΤΩΝ ΥΠΟΛΟΓΙΣΤΩΝ
    1ο Εξάμηνο
    Πανεπιστήμιο Πειραιώς

--- ask_3.py
+++ ask_3.py
@@ -13,12 +13,24 @@ fin.close()
 #Anoigma arxiou gia Write
 fin = open("hello_world.py","w")
 
+final_list=[]
+
 for line in lines:
+    counter1=0
+    counter2=0
     if "#" in line:
         y=list(line)
         x=y.index("#")
-        for i in range(0,x):
-            final_list.append(y[i])
+        for i in range(x,0,-1):
+            if line[i] == "'":
+                counter1+=1
+            elif line[i] == '"':
+                counter2+=1
+        if counter1 % 2 == 1 or counter2 % 2 == 1:
+            final_list.append(line)
+        else:
+            for i in range(0,x):
+                final_list.append(y[i])
         if line[0] != "#":
             final_list.append("\n")
     else:
